525bf17fd237d04d96b473ff180e9f92fc8c43e5
chandan0-1 <chandanofficial.cn@gmail.com>
2021-03-17 12:20:39 +0530

    Returning external HTML file as respose to the browser

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const http = require("http");
 const port = 8000;
+const fs = require("fs");
 
 function requestHandler(req,res){
 	console.log(req.url);//Printing the requested url by browser
 
-	res.end("<h1>Hello, from Server !!</h1>");//returning response to browser
+	res.writeHead(200,{"content-type" : "text/html"});
+
+	fs.readFile("index.html",function(err,data){
+		if (err){
+			console.log("Got error =>",err);
+			return res.end("<h2> Error Found!</h2>");
+		}
+		return res.end(data);
+	})
+
+
+	// res.end("<h1>Hello, from Server !!</h1>");//returning response to browser
 }
 
 
